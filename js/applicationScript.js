bcb3b3f035651752fee652a53b067851fdbf8523
CAEmario <CAEAddress@gmail.com>
2017-10-25 16:01:34 +0200

    Code regeneration/Model synchronization

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -40,7 +40,7 @@ var init = function() {
 
   };
 
-  client = new Las2peerWidgetLibrary("http://cloud10.dbis.rwth-aachen.de:8086/videoquiz/", iwcCallback);
+  client = new Las2peerWidgetLibrary("http://cloud10.dbis.rwth-aachen.de:8086/videoquiz", iwcCallback);
 
   $('#button_355e0').on('click', function() {
     //start parameter initiation
